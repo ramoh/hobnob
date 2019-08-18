1a1d8e6b9010b3bb4bce221179976a81ef634640
ramoh <mohantyrajesh1984@gmail.com>
2019-08-18 16:09:55 +0530

    Handle create user call with empty payload 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import '@babel/polyfill';
 import http from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const requestHandler = function (req, res) {
     console.log(req.connection.remoteAddress);
+    if (req.method === 'POST' && req.url === '/users') {
+        res.writeHead(400, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify({
+            message: 'Payload should not be empty',
+        }));
+        return;
+
+    }
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
@@ -10,4 +21,5 @@ const requestHandler = function (req, res) {
 };
 
 const server = http.createServer(requestHandler);
-server.listen(8080);
+server.listen(process.env.SERVER_PORT);
+console.log("Server has been started");
